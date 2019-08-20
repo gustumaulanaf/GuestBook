784c95a5cef04b933b112b550f66182f9a6ea8a5
gustumaulanaf <gustumaulanaf@gmail.com>
2019-08-20 14:04:05 +0700

    First

---
+++ app/src/main/java/com/example/tamu/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.example.tamu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    private  int waktuloading = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },waktuloading);
+    }
+}
